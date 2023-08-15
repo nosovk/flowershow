7c7062fd19d8f1f856ec193d67a44752bddddba1
Konstantin Nosov <nosovk@gmail.com>
2023-08-15 12:31:10 -0700

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -1,1 +1,29 @@
-export default {};
+export default {
+  title: "NodeArt Obs",
+  showEditLink: false,
+  showToc: true,
+  showSidebar: false,
+  showLinkPreviews: true,
+  author: "NodeArt",
+  domain: "https://obs.nodeart.ap",
+  // Google analytics key e.g. G-XXXX
+  analytics: "",
+  // content source directory for markdown files
+  // DO NOT CHANGE THIS VALUE
+  // if you have your notes in another (external) directory,
+  // /content dir should be a symlink to that directory
+  content: "content",
+  // avatarPlaceholder: "/_flowershow/avatarplaceholder.png",
+  // contentExclude: [],
+  // contentInclude: [],
+  // blogDir: "blog",
+  // peopleDir: "people",
+  // Theme
+  // theme: {
+    // default: "dark",
+    // toggleIcon: "/_flowershow/theme-button.svg",
+  // },
+  navLinks: [
+    // { href: '/about', name: 'About' },
+  ]
+};
